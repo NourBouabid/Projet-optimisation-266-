6197fb3f9f9e5a88ccf580952d6e614816b07c34
lumy10 <bouabidnour02@gmail.com>
2026-02-28 15:43:08 +0100

    methode2

--- algo2_3.py
+++ algo2_3.py
@@ -1,12 +1,29 @@
+import numpy as np
 from scipy.optimize import least_squares
 
 #fonction à minimiser
 def F(X,Y,xc,yc,r):
-    return sum([(((X[i]-xc)**2 (Y[i]-yc)**2)**0.5 - r)**2 for i in range(len(X))])
+    return sum([(((X[i]-xc)**2 + (Y[i]-yc)**2)**0.5 - r)**2 for i in range(len(X))])
+
+def residus(x,y,xc, yc, r):
+        return np.sqrt((x-xc)**2 + (y-yc)**2) - r
 
 #minimisation sans contrainte
-def methode2(X,Y):
-    return ...
+def methode2(x,y,method='lm',max_nfev=1000):
+    '''
+    minimise F sans contrainte
+    Datas (x,y)
+    par défaut methode Levenberg-Marquardt avec régularisation Ridge
+    '''
+    # initialisation
+    xc0 = np.mean(x)
+    yc0 = np.mean(y)
+    r0  = np.mean(residus(x,y,xc0, yc0, r0))
+
+    result = least_squares(residus, [xc0, yc0, r0], method, max_nfev)  #max nombre d'itération1000
+
+    return result.x
+#LM est une version améliorée de la methode de Gausse-Newton, étudier la convergence de la suite des paramêtres
 
 #minimisation avec contrainte
 def methode3(X,Y):
